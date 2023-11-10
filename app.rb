ffb49e278a0da86d50c7019a92604fd4ba49c79d
Luke O'Connell <147653898+lukeoc615@users.noreply.github.com>
2023-11-10 15:56:28 +0000

    latitude and longitude working

--- app.rb
+++ app.rb
@@ -1,9 +1,26 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
 
 get("/") do
   "
   <h1>Welcome to your Sinatra App!</h1>
   <p>Define some routes in app.rb</p>
   "
+end
+
+get("/umbrella") do
+  erb(:umbrella_form)
+end
+
+get("/process_umbrella") do
+  @user_location = params.fetch("user_loc")
+  
+  gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=Merchandise%20Mart%20Chicago&key=AIzaSyDKz4Y3bvrTsWpPRNn9ab55OkmcwZxLOHI"
+  @raw_response = HTTP.get(gmaps_url).to_s
+  @parsed_response = JSON.parse(@raw_response)
+  @loc_hash = @parsed_response.dig("results", 0, "geometry", "location")
+  @latitude = @loc_hash.fetch("lat")
+  @longitude = @loc_hash.fetch("lng")
+  erb(:umbrella_results)
 end
