58277fdb13585d332f2c0b68c7d9c672ae6b3a40
Luke O'Connell <147653898+lukeoc615@users.noreply.github.com>
2023-11-10 16:31:00 +0000

    added cookies

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require "sinatra"
 require "sinatra/reloader"
 require "http"
+require "sinatra/cookies"
 
 get("/") do
   "
@@ -13,14 +14,19 @@ get("/umbrella") do
   erb(:umbrella_form)
 end
 
-get("/process_umbrella") do
+post("/process_umbrella") do
   @user_location = params.fetch("user_loc")
   
-  gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=Merchandise%20Mart%20Chicago&key=AIzaSyDKz4Y3bvrTsWpPRNn9ab55OkmcwZxLOHI"
+  url_encoded_string = @user_location.gsub(" ", "+")
+  gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{url_encoded_string}&key=AIzaSyDKz4Y3bvrTsWpPRNn9ab55OkmcwZxLOHI"
   @raw_response = HTTP.get(gmaps_url).to_s
   @parsed_response = JSON.parse(@raw_response)
   @loc_hash = @parsed_response.dig("results", 0, "geometry", "location")
   @latitude = @loc_hash.fetch("lat")
   @longitude = @loc_hash.fetch("lng")
+  
+  cookies["last_location"] = @user_location
+  cookies["last_lat"] = @latitude
+  cookies["last_lng"] = @longitude
   erb(:umbrella_results)
 end
